89da955f7bb64daa5d4938ad5b1689081fd56e1c
itemokgeng <44459666+itemokgeng@users.noreply.github.com>
2023-05-22 13:11:23 +0200

    Add files via upload

---
+++ Core/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Interfaces
+{
+  public interface IRepository<T>
+    where T : class, new()
+  {
+    // only the update is let for the CRUD
+    //T Entity Create(); 
+    T Post(T entity);
+
+    IEnumerable<T> GetAll();
+
+    //T Entity GetByID(); 
+    T GetByID(int id);
+
+    //T Entity Delete(); 
+    T Delete(T entity);
+
+    T Update(T entity);
+  }
+}
