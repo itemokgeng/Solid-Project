e392c4201371f4ddd4ffbb44377bf5be627b1dbc
itemokgeng <44459666+itemokgeng@users.noreply.github.com>
2023-05-22 13:14:21 +0200

    Add files via upload

---
+++ Domain/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+  // one class 
+  // Single Responsibility  
+  public class Department
+  {
+    public int DepartmentID { get; set; }
+    public string DepartmentName { get; set; } 
+  }
+}
---
+++ Domain/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+  // one class 
+  // Single Responsibility  
+  public class Employee
+  {
+    public int EmployeeID { get; set; }
+    public string EmployeeName { get; set; }
+    public string EmployeeSurname { get; set; }
+    // forigen key 
+    public int DepartmentID { get; set; }
+  }
+}
